12d342b6c2e2085e5e8c056db38c3d087e82fda3
Juan J. Barriga <jubaga95@gmail.com>
2022-03-22 01:31:33 +0100

    ajustando responsive

--- utils/js/catalogo.js
+++ utils/js/catalogo.js
@@ -10,112 +10,34 @@ let price;
 let tipo;
 let contCartas;
 
-window.addEventListener("load", () => {
-    dispositivos = document.querySelector("#dispositivos");
-    elementos = document.querySelector("#elementos");
-    descripcion = document.querySelector("#descripcion");
-    imagen = document.querySelector("#imagen");
-
-    // $(document).on('click', '.dropdown-menu li a', function () {
-    //     $('#dispositivos').val($(this).html());
-    // });
-    // dispositivos.addEventListener("click", (e) => {
-    //     activo = $('#dispositivos').val();
-    //     console.log("activo", activo);
-    //     itemSeleccionado = e.target.value;
-    //     cargarDispositivos(itemSeleccionado);
-    // })
-    // elementos.addEventListener("click", (e) => {
-    //     dispositivoSeleccionado = e.target.value;
-    //     cargarContenido(dispositivoSeleccionado);
-    //     cargarImagen(dispositivoSeleccionado);
-    // })
-})
-
-$(document).ready(function () {
-
-    // $("#dispositivos").change(function (e) {
-    //     id = $("#dispositivos").val();
-    //     console.log("id", id);
-    //     url = `https://test.krama.es:8014/item/list/${id}`;
-    //     $.ajax({
-    //         type: "GET",
-    //         url: url,
-    //         data: id,
-    //         dataType: "json",
-    //         success: function (response) {
-    //             response.forEach((element) => {
-    //                 let codigoCarta = `<div class="col" id="div-elementos">
-    //                 <div class="card">
-    //                     <img src="${element.pictureUrl}" class="card-img-top" alt="...">
-    //                     <div class="card-body">
-    //                     <h5 class="card-title">${element.description}</h5>
-    //                     <div>
-    //                         <ul class="list-group">
-    //                             <li class="list-group-item">${element.price} €</li>
-    //                             <li class="list-group-item">
-    //                                 <a class="list-group-item list-group-item-action
-    //                                     active" id="btn_detalle" data-toggle="list" 
-    //                                     href="#" role="tab" aria-controls="detalle" 
-    //                                     onclick="cargarDetalle(${element.id})">
-    //                                     Detalle
-    //                                 </a>
-    //                             </li>
-    //                         </ul>
-    //                     </div>
-    //                     </div>
-    //                 </div>
-    //                 </div>`;
-    //                 $(".card").fadeIn();
-    //                 $("#contenedor_cartas").append(codigoCarta);
-    //             });
-    //         },
-    //     });
-    // });
-    $("#dispositivos").change(function () {
-        $("#contenedor_cartas ").html("");
-        $("#contenedor_detalle ").html("");
-    })
-});
-
-function cargarElementos(id){
+function cargarElementos(id) {
     console.log("id", id);
-        url = `https://test.krama.es:8014/item/list/${id}`;
-        $.ajax({
-            type: "GET",
-            url: url,
-            data: id,
-            dataType: "json",
-            success: function (response) {
-                response.forEach((element) => {
-                    let codigoCarta = `<div class="col" id="div-elementos">
-                    <div class="card">
-                        <img src="${element.pictureUrl}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                        <h5 class="card-title">${element.description}</h5>
-                        <div>
-                            <ul class="list-group">
-                                <li class="list-group-item">${element.price} €</li>
-                                <li class="list-group-item">
-                                    <a class="list-group-item list-group-item-action
-                                        active" id="btn_detalle" data-toggle="list" 
-                                        href="#" role="tab" aria-controls="detalle" 
-                                        onclick="cargarDetalle(${element.id})">
-                                        Detalle
-                                    </a>
-                                </li>
-                            </ul>
+    url = `https://test.krama.es:8014/item/list/${id}`;
+    $.ajax({
+        type: "GET",
+        url: url,
+        data: id,
+        dataType: "json",
+        success: function (response) {
+            response.forEach((element) => {
+                let codigoCarta =
+                    `<div class="col" id="div-elementos">
+                        <div class="card d-flex">
+                            <img class="card-img-top responsive" src="${element.pictureUrl}" alt="Card image" id="img_card">
+                            <div class="card-body">
+                                <h5 class="card-title" id="title_card">${element.description}</h4>
+                                <h5 class="card-text" id="price_card">${element.price} €</h5>
+                                <a href="#" class="btn btn-secondary" onclick="cargarDetalle(${element.id})" id="btn_detalle">Detalle</a>
+                            </div>
                         </div>
-                        </div>
-                    </div>
                     </div>`;
-                    $(".card").fadeIn();
-                    $("#contenedor_cartas").append(codigoCarta);
-                });
-            },
-        });
-        $("#contenedor_cartas ").html("");
-        $("#contenedor_detalle ").html("");
+                $(".card").fadeIn();
+                $("#contenedor_cartas").append(codigoCarta);
+            });
+        },
+    });
+    $("#contenedor_cartas ").html("");
+    $("#contenedor_detalle ").html("");
 }
 
 function cargarDetalle(id) {
@@ -131,8 +53,8 @@ function cargarDetalle(id) {
         success: function (response) {
             console.log("resultados contenido: ", response);
             let detalleItem = `
-                    <div class="w3-container w3-padding-32 w3-center">
-                    <img src="${response.pictureUrl}" class="w3-image">
+                    <div class="w3-container w3-padding-32 w3-center col-md-6">
+                    <img src="${response.pictureUrl}" class="w3-image responsive" id="img_detalle">
                         <div class="w3-padding-32">
                             <h4><b>${response.description}</b></h4>
                             <div class="progress">
@@ -152,10 +74,6 @@ function cargarDetalle(id) {
 
         },
     });
-    
-    var bar1 = new ldBar("#progBar");
-    var bar2 = document.getElementById('progBar').ldBar;
-    bar1.set(60);
-    
+
 
 }
